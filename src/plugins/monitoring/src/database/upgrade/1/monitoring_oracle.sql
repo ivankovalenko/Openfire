f19b98a80fb193b188df7c9dbf12e375c5e245a4
Ivan Kovalenko <ivan.kovalenko@gmail.com>
2016-01-29 19:17:00 +0300

    changed to varchar2
    due to vendor recommendation

--- src/plugins/monitoring/src/database/upgrade/1/monitoring_oracle.sql
+++ src/plugins/monitoring/src/database/upgrade/1/monitoring_oracle.sql
@@ -1,8 +1,8 @@
 -- $Revision$
 -- $Date$
 
-ALTER TABLE ofMessageArchive ADD fromJIDResource VARCHAR(255) NULL;
-ALTER TABLE ofMessageArchive ADD toJIDResource VARCHAR(255) NULL;
+ALTER TABLE ofMessageArchive ADD fromJIDResource VARCHAR2(255) NULL;
+ALTER TABLE ofMessageArchive ADD toJIDResource VARCHAR2(255) NULL;
 
 -- Update database version
 UPDATE ofVersion SET version = 1 WHERE name = 'monitoring';
