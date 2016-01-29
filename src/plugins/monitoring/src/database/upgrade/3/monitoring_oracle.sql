457dfbe4683d8a29b0efefcfc755ec00113f2a08
Ivan Kovalenko <ivan.kovalenko@gmail.com>
2016-01-29 19:15:59 +0300

    Update monitoring_oracle.sql

--- src/plugins/monitoring/src/database/upgrade/3/monitoring_oracle.sql
+++ src/plugins/monitoring/src/database/upgrade/3/monitoring_oracle.sql
@@ -2,7 +2,7 @@
 -- $Date$
 
 ALTER TABLE ofMessageArchive ADD messageID INTEGER NULL;
-ALTER TABLE ofMessageArchive ADD stanza LONG NULL;
+ALTER TABLE ofMessageArchive ADD stanza CLOB NULL;
 
 -- Update database version
 UPDATE ofVersion SET version = 3 WHERE name = 'monitoring';
